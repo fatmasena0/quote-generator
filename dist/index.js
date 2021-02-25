0d07cc513bc3012bdd2c1a1f0505364bc7de48f3
fsakcglyn <44836864+fsakcglyn@users.noreply.github.com>
2021-02-25 15:44:52 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,9 @@
 function fetchQuote() {
 	fetch("https://favqs.com/api/qotd")
 	.then((response) => response.json())
-	.then((data) => (document.getElementById("quote").innerHTML = data.quote.body));
-	fetch("https://favqs.com/api/qotd")
-	.then((response) => response.json())	
-	.then((data) => (document.getElementById("author").innerHTML = data.quote.author));
-
+	.then((data) => (
+		(document.getElementById("quote").innerHTML = data.quote.body),
+		(document.getElementById("author").innerHTML = data.quote.author)
+		)
+	);
 }
